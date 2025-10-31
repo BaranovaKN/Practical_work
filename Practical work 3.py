52bbf2842286dffef7a55edcac3ab59c7b9923b1
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2025-11-01 02:09:58 +0300

    not_task_6

--- Practical work 3.py
+++ Practical work 3.py
@@ -65,3 +65,23 @@ print(unique_items)
 
 #Задание 6
 
+# 1. Сбор данных от пользователя
+name = input("Ваше имя: ")
+age = int(input())
+subject_str = input("введите ваши любимые предметы через запятую")
+
+# 2. Преобразование subject_str в subject_list
+subject_list = [subject_str.strip() for subject_str in subject_str.split(",")]
+
+# 3. Создание словаря
+student = {
+    "Имя":name,
+    "Возраст":age,
+    "Список любимых предметов":subject_list
+}
+
+# 4. Красивый вывод анкеты
+print("="*40)
+print("АНКЕТА СТУДЕНТА")
+print("="*40)
+print(f"Имя")
