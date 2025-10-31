ea40b127a592c13906ec3ca134f1bf3b829b733b
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2025-11-01 01:45:50 +0300

    without_task_6

--- Practical work 3.py
+++ Practical work 3.py
@@ -2,6 +2,7 @@
 # Задание 1
 
 print("Task 1")
+
 a = 42
 b = 3.14159
 c = "Hello, Python!"
@@ -11,5 +12,56 @@ f = (4, 5, 6)
 g = {"name": "Alice", "age": 30}
 h = {7, 8, 9}
 i = None
+
 print(f"""a это {type(a)} \nb это {type(b)} \nc это {type(c)} \nd это {type(d)} \n"""
       f"""e это {type(e)} \nf это {type(f)} \ng это {type(g)} \nh это {type(h)} \ni это {type(i)}""")
+
+
+# Задание 2
+
+# список
+my_list = [1, 2, 3]
+print(my_list)
+
+my_list[1] = 100
+print(my_list)
+
+#кортеж
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+try:
+    my_tuple[1]=100
+except TypeError:
+    print("Ошибка! Нельзя изменять значения кортежа")
+
+#строка
+my_string = "cat"
+print(my_string)
+
+try:
+    my_string[0]='b'
+except TypeError:
+    print("Ошибка! Нельзя изменять символ строки!")
+
+#Задание 3
+
+# try:
+#     num1 = input("Введите первое число: ")
+#     num1 = int(num1)
+#     num2 = int(input("Введите второе число: "))
+#     print(num1 + num2)
+# except ValueError:
+#     print("Ошибка! Введено некорректное значение!")
+
+#Задание 5
+
+shopping_list = ["apple", "milk", "eggs", "tomato"]
+shopping_list.append("milk")
+print("Длинна списка равна: ",len(shopping_list))
+
+unique_items = set(shopping_list)
+print(unique_items)
+
+#Задание 6
+
