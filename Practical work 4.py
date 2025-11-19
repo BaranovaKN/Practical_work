8b200f3ff8243ab95b0d857c5e221ce38e551679
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2025-11-19 13:22:39 +0300

    practical_work_4v2.1

--- Practical work 4.py
+++ Practical work 4.py
@@ -58,6 +58,6 @@ print("результат вычиления функции: ", result)
 num_seat = int(input("введите номер места: "))
 place_in_coupe = 4
 
-num_coupe = math.ceil(num_seat/4)
+num_coupe = math.ceil(num_seat/place_in_coupe)
 
 print(f"при номере места {num_seat} пассажир будет в {num_coupe} купе")
