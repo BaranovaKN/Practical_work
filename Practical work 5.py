edd820f54c14568b8c0d50b230e3d0beff123c49
BaranovaKN <christbtomsk@gmail.com>
2025-12-22 07:24:51 +0300

    commit 5v3

--- Practical work 5.py
+++ Practical work 5.py
@@ -145,6 +145,29 @@ c = calculate_distance(A_x, A_y, C_x, C_y)
 triangle_area = calculate_triangle_area(a, b, c)
 
 print(f"Площадь треугольника - {triangle_area:.2f}, стороны треугольника - {a:.2f}; {b:.2f}; {c:.2f}")
+
 # Задание 7
 
+def fuel_consumption_calculation(distance, fuel_consumption_per_100_km):
+    """
+    Вычисляет количество топлива которое понадобится в дороге и его цену.
+
+    Args:
+        distance (float): Расстояние, которое планируется проехать.
+        fuel_consumption_per_100_km (float): Расход топлива за 100 км.
+
+    Returns:
+        float: Количество топлива которое необходимо для преодоления пути и цена этого топлива
+    """
+    FUEL_PRICE = 49.5
+    amount_fuel = (distance/100)* fuel_consumption_per_100_km
+    total_fuel_price = amount_fuel * FUEL_PRICE
+    return amount_fuel, total_fuel_price
+
+distance = float(input("Введите расстояние, которое планируете проехать на машине: "))
+fuel_consumption_per_100_km = float(input("Введите расход топлива в вашей машине на 100 км в литрах: "))
+
+amount_fuel, total_fuel_price = fuel_consumption_calculation(distance, fuel_consumption_per_100_km)
+
+print(f"Для такой поездки понадобится - {amount_fuel:.2f} л топлива и это будет стоить - {total_fuel_price:.2f} руб.")
 
