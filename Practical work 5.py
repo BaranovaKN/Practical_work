f068439e279c815ad138666873346a3f7532a198
BaranovaKN <christbtomsk@gmail.com>
2025-12-22 06:55:53 +0300

    commit 5v2

--- Practical work 5.py
+++ Practical work 5.py
@@ -2,104 +2,105 @@ import math
 
 # Задание 1
 
-#     annual_income = float(input("Введите свой годовой доход: "))
-#     TAX_RATE = 0.13
-#
-#     if annual_income >= 0:
-#         tax_amount = annual_income * TAX_RATE
-#         income_after_tax = annual_income - tax_amount
-#
-#         print(f"\nВаша общая сумма дохода - {annual_income:,.2f} руб. \nСумма вашего налога - {tax_amount:,.2f} руб. "
-#               f"\nВаш доход после уплаты налогов - {income_after_tax:,.2f} руб.")
+annual_income = float(input("Введите свой годовой доход: "))
+TAX_RATE = 0.13
 
-#     else:
-#         print("\nОшибка! Доход должен быть больше или равен 0")
+if annual_income >= 0:
+    tax_amount = annual_income * TAX_RATE
+    income_after_tax = annual_income - tax_amount
+
+    print(f"\nВаша общая сумма дохода - {annual_income:,.2f} руб. \nСумма вашего налога - {tax_amount:,.2f} руб. "
+          f"\nВаш доход после уплаты налогов - {income_after_tax:,.2f} руб.")
+
+else:
+    print("\nОшибка! Доход должен быть больше или равен 0")
+
 # Задание 2
-#
-#     height, weight = map(float, input("Пожалуйста введите ваш рост(в метрах) "
-#                                     "и вес (в кг) в одной строке через пробел: ").split())
-#     bmi = weight / (height * height)
-#     print(f"\nваш Индекс Массы Тела - {bmi:.1f}")
+
+height, weight = map(float, input("Пожалуйста введите ваш рост(в метрах) "
+                                  "и вес (в кг) в одной строке через пробел: ").split())
+bmi = weight / (height * height)
+print(f"\nваш Индекс Массы Тела - {bmi:.1f}")
 
 # Задание 3
-#
-# def convert_usd_to_rub(amount_usd):
-#     """
-#     Конвертирует сумму в долларах в рубли.
-#
-#     Args:
-#          amount_usd (int): сумма в долларах.
-#
-#     Returns:
-#         float: сумму в рублях.
-#     """
-#     USD_TO_RUB = 95.50
-#     return USD_TO_RUB * amount_usd
-#
-# amount_usd = int(input("введите сумму в долларах: "))
-# amount_rub = convert_usd_to_rub(amount_usd)
-# print(f"Ваша сумма в долларах равна - {amount_rub} руб.")
+
+def convert_usd_to_rub(amount_usd):
+    """
+    Конвертирует сумму в долларах в рубли.
+
+    Args:
+         amount_usd (int): сумма в долларах.
+
+    Returns:
+        float: сумму в рублях.
+    """
+    USD_TO_RUB = 95.50
+    return USD_TO_RUB * amount_usd
+
+amount_usd = int(input("введите сумму в долларах: "))
+amount_rub = convert_usd_to_rub(amount_usd)
+print(f"Ваша сумма в долларах равна - {amount_rub} руб.")
 
 # Задание 4
 
-# def calculate_rectangle_area(width, height):
-#     """
-#
-#     Вычисляет площадь прямоугольника.
-#
-#     Args:
-#         width (int): Ширина прямоугольника.
-#         height (int): Высота прямоугольника.
-#
-#     Returns:
-#         int: Площадь прямоугольника.
-#     """
-#     return width* height
-#
-# def calculate_circle_area(radius):
-#     """
-#     Args:
-#         radius (int): радиус круга
-#
-#     Returns:
-#         float: площадь круга
-#     """
-#     return math.pi * radius**2
-#
-# width, height = map(int, input("Введите ширину и высоту прямоугольника через пробел: ").split())
-# rectangle_area = calculate_rectangle_area(width, height)
-# print(f"Площадь прямоугольника - {rectangle_area}")
-#
-# radius = int(input("Введите радиус круга: "))
-# circle_area = calculate_circle_area(radius)
-# print(f"Площадь круга - {circle_area}")
+def calculate_rectangle_area(width, height):
+    """
+
+    Вычисляет площадь прямоугольника.
+
+    Args:
+        width (int): Ширина прямоугольника.
+        height (int): Высота прямоугольника.
+
+    Returns:
+        int: Площадь прямоугольника.
+    """
+    return width* height
+
+def calculate_circle_area(radius):
+    """
+    Args:
+        radius (int): радиус круга
+
+    Returns:
+        float: площадь круга
+    """
+    return math.pi * radius**2
+
+width, height = map(int, input("Введите ширину и высоту прямоугольника через пробел: ").split())
+rectangle_area = calculate_rectangle_area(width, height)
+print(f"Площадь прямоугольника - {rectangle_area}")
+
+radius = int(input("Введите радиус круга: "))
+circle_area = calculate_circle_area(radius)
+print(f"Площадь круга - {circle_area}")
 
 # Задание 5
-#
-# B5000 = 5000
-# B2000 = 2000
-# B1000 = 1000
-# B500 = 500
-# B200 = 200
-# B100 = 100
-#
-# amount_money = int(input("Введите количество денег которое хотите снять(кратно 100): "))
-#
-# number_b5000 = amount_money//5000
-# rest_money = amount_money%5000
-# number_b2000 = rest_money//2000
-# rest_money = rest_money%2000
-# number_b1000 = rest_money//1000
-# rest_money = rest_money%1000
-# number_b500 = rest_money//500
-# rest_money = rest_money%500
-# number_b200 = rest_money//200
-# rest_money = rest_money%200
-# number_b100 = rest_money//100
-#
-# print(f"Если вы снимете - {amount_money} руб. вы получите купюр: "
-#       f"\nкупюр по 5000 - {number_b5000}\nкупюр по 2000 - {number_b2000}\nкупюр по 1000 -{number_b1000}"
-#       f"\nкупюр по 500 - {number_b500}\nкупюр по 200 - {number_b200}\nкупюр по 100 - {number_b100}")
+
+B5000 = 5000
+B2000 = 2000
+B1000 = 1000
+B500 = 500
+B200 = 200
+B100 = 100
+
+amount_money = int(input("Введите количество денег которое хотите снять(кратно 100): "))
+
+number_b5000 = amount_money//5000
+rest_money = amount_money%5000
+number_b2000 = rest_money//2000
+rest_money = rest_money%2000
+number_b1000 = rest_money//1000
+rest_money = rest_money%1000
+number_b500 = rest_money//500
+rest_money = rest_money%500
+number_b200 = rest_money//200
+rest_money = rest_money%200
+number_b100 = rest_money//100
+
+print(f"Если вы снимете - {amount_money} руб. вы получите купюр: "
+      f"\nкупюр по 5000 - {number_b5000}\nкупюр по 2000 - {number_b2000}\nкупюр по 1000 -{number_b1000}"
+      f"\nкупюр по 500 - {number_b500}\nкупюр по 200 - {number_b200}\nкупюр по 100 - {number_b100}")
 
 # Задание 6
 
