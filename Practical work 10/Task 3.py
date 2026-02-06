2acc15259c873b9ae381a56d2f0cc097a25aaae7
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2026-02-06 14:53:46 +0300

    Update Task 3.py

--- Practical work 10/Task 3.py
+++ Practical work 10/Task 3.py
@@ -1,4 +1,3 @@
-from typing import final
 
 print("Рекордсмен")
 
@@ -7,12 +6,15 @@ FINAL_NUM = 0
 
 while True:
     try:
-        user_num = int(input("Введите ваше число: "))
+        user_num = int(input("Введите ваше натуральное число: "))
     except ValueError:
         print("Ошибка! Введены некорректные данные!")
     else:
         if user_num == 0:
             print(f"Введенное вами максимальное число: {MAX_NUM}")
             break
+        elif 0 > user_num:
+            print("Ошибка! Число отрицательное!")
         elif MAX_NUM == None or user_num > MAX_NUM:
             MAX_NUM = user_num
+
