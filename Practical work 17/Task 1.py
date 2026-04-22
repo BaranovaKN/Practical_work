97130aa07644f67947f027c5d7bc118b4de7dcf6
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2026-04-22 12:45:32 +0300

    Update Task 1.py

--- Practical work 17/Task 1.py
+++ Practical work 17/Task 1.py
@@ -19,25 +19,3 @@ while True:
             print('Месяцев всего 12!')
 
 print(f'В {user_month} месяце: {get_days(user_month)} дней')
-
-def get_days(user_month):
-    match user_month:
-        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
-            return 31
-        case 4 | 6 | 9 | 11:
-            return 30
-        case 2:
-            return 28
-
-while True:
-    try:
-        user_month = int(input('Введите ваш месяц: '))
-    except ValueError:
-        print('ОШИБКА! ВВЕДЕНО НЕКОРРЕКТНОЕ ЗНАЧЕНИЕ!')
-    else:
-        if 13 > user_month > 0:
-            break
-        else:
-            print('Месяцев всего 12!')
-
-print(f'В {user_month} месяце: {get_days(user_month)} дней')
