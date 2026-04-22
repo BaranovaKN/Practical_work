7b521dc76517d817300dbc2a6d5b800e08a18bbc
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2026-04-22 12:39:01 +0300

    Create Task 2.py

---
+++ Practical work 17/Task 2.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(user_num):
+    if user_num < 2:
+        return False
+    for i in range(2, int(math.sqrt(user_num)) + 1):
+        if user_num % i == 0:
+            return False
+    return True
+
+print(is_prime(1))
+print(is_prime(10))
+print(is_prime(17))
