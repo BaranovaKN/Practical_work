0e9bd7339fe05ff0add8a0fa8e3d3bc85dd12765
BaranovaKN <christbtomsk@gmail.com>
2026-05-17 03:28:25 +0300

    1605

---
+++ 1605/Task 3.py
@@ -0,0 +1,42 @@
+class Block():
+    def __init__(self):
+        self.name = None
+        self.hardness = 0
+        self.breakable = False
+
+    def show_info(self):
+        print(f'Блок: {self.name}\nПрочность: {self.hardness}\nМожно сломать: {self.breakable}\n\n')
+
+
+class Stone(Block):
+    def set_properties(self):
+        self.name = 'Stone'
+        self.hardness = 10
+        self.breakable = True
+
+
+class Bedrock(Block):
+    def set_properties(self):
+        self.name = 'Bedrock'
+        self.hardness = 999
+        self.breakable = False
+
+
+class GrassBlock(Block):
+    def set_properties(self):
+        self.name = 'GrassBlock'
+        self.hardness = 1
+        self.breakable = True
+
+
+stone = Stone()
+bedrock = Bedrock()
+grass = GrassBlock()
+
+stone.set_properties()
+bedrock.set_properties()
+grass.set_properties()
+
+stone.show_info()
+bedrock.show_info()
+grass.show_info()
